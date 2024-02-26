6bef9f480f8790a88f9ac7da3757b4b2df6ea85b
CozyLilKitty <cozylilkitty@gmail.com>
2024-02-26 22:17:18 +0200

    fixed the preprocessor

--- grade_predictor.py
+++ grade_predictor.py
@@ -11,91 +11,44 @@ def main():
 	st.title('Mathematics Grade Predictor')
 
 	# Buttons to select options
-	sex = st.radio(
-    "What's the sex of the student?",
-    ["Female", "Male"])
-	# Changing the data to a format the model will recognize
-	if sex == "Female":
-		sex = "F"
-	else:
-		sex = "M"
-
-	address = st.radio(
-    "What area does the student live in?",
-    ["Rural", "Urban"])
-
-	if address == "Rural":
-		address = "R"
-	else:
-		address = "U"
-
+	age = st.radio(
+    "What's the age of the student?",
+    [15, 16, 17, 18, 19, 20, 21, 22])
+	
 	Medu = st.radio(
 	"What is the highest level of education the student's mother achieved?",
 	["None","Primary School","5th to 9th Grade", "Finished Secondary","Higher Education"])
-
+	# Changing the data to a format the model will recognize
 	if Medu == "None":
-		Medu = "0"
+		Medu = 0
 	elif Medu == "Primary School":
-		Medu = "1"
+		Medu = 1
 	elif Medu == "5th to 9th Grade":
-		Medu = "2"
+		Medu = 2
 	elif Medu == "Finished Secondary":
-		Medu = "3"
+		Medu = 3
 	else:
-		Medu = "4"
+		Medu = 4
 
 	Fedu = st.radio(
 	"What is the highest level of education the student's father achieved?",
 	["None","Primary School","5th to 9th Grade", "Finished Secondary","Higher Education"])
 
 	if Fedu == "None":
-		Fedu = "0"
+		Fedu = 0
 	elif Fedu == "Primary School":
-		Fedu = "1"
+		Fedu = 1
 	elif Fedu == "5th to 9th Grade":
-		Fedu = "2"
+		Fedu = 2
 	elif Fedu == "Finished Secondary":
-		Fedu = "3"
+		Fedu = 3
 	else:
-		Fedu = "4"
+		Fedu = 4
 
-	Mjob = st.radio(
-	"What does the student's mother do for work?",
-	["Teacher","Healthcare-related","Civil Services", "Stay At Home","Other"])
-
-	if Mjob == "Teacher":
-		Mjob = "teacher"
-	elif Mjob == "Healthcare-related":
-		Mjob = "health"
-	elif Mjob == "Civil Services":
-		Mjob = "services"
-	elif Mjob == "Stay At Home":
-		Mjob = "at_home"
-	else:
-		Mjob = "other"
-
-	reason = st.radio(
-	"What was the reason for choosing this school?",
-	["Close to Home", "School Reputation", "Course Preference", "Other"]
+	failures = st.radio(
+	"How many times has the student failed the class?",
+	[0,1,2,3,4]
 	)
-
-	if reason == "Close to Home":
-		reason = "home"
-	elif reason == "School Reputation":
-		reason = "reputation"
-	elif reason == "Course Preference":
-		reason = "preference"
-
-
-	paid = st.radio(
-	"Does the student take extra paid classes for Mathematics?",
-	["Yes","No"]
-	)
-
-	if paid == "Yes":
-		paid = "yes"
-	else:
-		paid = "no"
 
 	higher = st.radio(
 	"Does the student want to pursue higher education?",
@@ -108,27 +61,25 @@ def main():
 		higher = "no"
 	
 	# Collecting all the chosen options into a list
-	result = [sex, address, Medu, Fedu, Mjob, reason, paid, higher]
+	result = [age, Medu, Fedu, failures, higher]
 	# Preparing the list to be turned into a dataframe
-	result = np.reshape(result, (8, 1)).T
+	result = np.reshape(result, (5, 1)).T
 	# Turning the list into a dataframe
-	df = pd.DataFrame(result)
-
-	# "unpickling" (opening) the OneHotEncoder
+	df = pd.DataFrame(result, columns=['age', 'Medu', 'Fedu', 'failures','higher'])
+	# "unpickling" (opening) the preprocessor
 	model_load_path = "encoder.pkl"
 	with open(model_load_path,'rb') as file:
 		encoder = pickle.load(file)
 
-	# Using the OneHotEncoder on our list
-	result = encoder.transform(df)
-
+	# Using the preprocessor on our list
+	preproc = encoder.transform(df)
 	# "unpickling" (opening) the trained linear model
 	model_load_path = "lr_model.pkl"
 	with open(model_load_path,'rb') as file:
 		unpickled_model = pickle.load(file)
 
 	# Predicting the grade with the model
-	prediction = unpickled_model.predict(result)
+	prediction = unpickled_model.predict(preproc)
 
 	# Changing the prediction to a 100 point scale so South Africans can understand
 	prediction = prediction * 5
